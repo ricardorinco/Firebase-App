a80aed60c2537c3e90d6cf518cc10961c9295f93
Ricardo Rinco <ricardo.rinco@hotmail.com>
2017-11-29 18:25:19 -0200

    Implementação de GitHub e Email Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,11 +15,30 @@ export class AppComponent {
 
   user: Observable<firebase.User>;
 
+  email: string;
+  password: string;
+
   constructor(private angularFireAuth: AngularFireAuth) {
     this.user = this.angularFireAuth.authState;
   }
 
   loginFacebook() {
     this.angularFireAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+  }
+
+  loginGitHub() {
+    this.angularFireAuth.auth.signInWithPopup(new firebase.auth.GithubAuthProvider());
+  }
+
+  loginEmail() {
+    firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+      .catch((error: any) => { console.log(error); } );
+
+    this.email = '';
+    this.password = '';
+  }
+
+  signOut() {
+    this.angularFireAuth.auth.signOut();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -19,6 +20,7 @@ import { environment } from './../environments/environment';
     AngularFireAuthModule,
     AngularFireDatabaseModule,
     BrowserModule,
+    FormsModule,
     NgbModule.forRoot()
   ],
   providers: [],
