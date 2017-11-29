e624033d20e2054d933e62400e3961ad693bebf7
Ricardo Rinco <ricardo.rinco@hotmail.com>
2017-11-29 17:30:36 -0200

    Implementação de AuthFacebook

--- README.md
+++ README.md
@@ -6,3 +6,13 @@
 This is a study on Login Social with Firebase.
 
 ![](https://1.bp.blogspot.com/-YIfQT6q8ZM4/Vzyq5z1B8HI/AAAAAAAAAAc/UmWSSMLKtKgtH7CACElUp12zXkrPK5UoACLcB/s1600/image00.png)
+
+# Installation
+
+git clone https://github.com/guiferreira/Angular4-com-Bootstrap4.git
+
+```sh
+$ npm install -g @angular/cli
+$ npm install
+$ ng serve --open
+```
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
@@ -8,4 +13,13 @@ export class AppComponent {
 
   title: string = 'Firebase';
 
+  user: Observable<firebase.User>;
+
+  constructor(private angularFireAuth: AngularFireAuth) {
+    this.user = this.angularFireAuth.authState;
+  }
+
+  loginFacebook() {
+    this.angularFireAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+  }
 }
